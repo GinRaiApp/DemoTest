79f0b33cc515aa614e67789638da49058be542d2
CholtichaWa <CholtichaWa@users.noreply.github.com>
2017-08-07 18:15:14 +0700

    save pdf

---
+++ html/pdf.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PDF Test</title>
+</head>
+<body>
+	<form name="form1" method="post" action="PageUploadToMySQL2.php" enctype="multipart/form-data">
+	<input type="file" name="filUpload"><br><br>
+	<input name="btnSubmit" type="submit" value="Submit">
+</body>
+</html>
